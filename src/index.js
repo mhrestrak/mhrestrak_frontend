02da2dd24d612949ed4441746b263df6454664eb
Joshua Ferndes <fernandes.developer@gmail.com>
2023-03-10 17:24:41 +0530

    Copy from master to old_branch

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+require('dotenv').config();
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
